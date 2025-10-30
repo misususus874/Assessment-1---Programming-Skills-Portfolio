22e333a3770ea1981a7797f233c099271f7cbff7
misususus874 <youngmikaela0708@gmail.com>
2025-10-30 21:37:18 +0400

    Update Exercise 8 - Simple Search.py
    Added a comma and capitalized the first letter of the user input

--- Exercise 8 - Simple Search.py
+++ Exercise 8 - Simple Search.py
@@ -1,7 +1,7 @@
-name_list = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
+name_list = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
 user = input("Enter a name: ")
-if user in name_list:
+if user.capitalize() in name_list:
     print(f"{user} is here!")
 else:
     print("Person not found!")
